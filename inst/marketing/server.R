ea15fcf847f3bfe2fa2a0c9b8a86b1842e5becfc
Vincent <vnijs@rady.ucsd.edu>
2014-06-30 14:36:36 -0700

    sourceDirectotry with modifiedOnly = FALSE
    https://github.com/rstudio/shiny/issues/524

--- inst/marketing/server.R
+++ inst/marketing/server.R
@@ -5,9 +5,9 @@ shinyServer(function(input, output, session) {
 	source('radiant.R', local = TRUE)
 
 	# source data & analysis tools
-	R.utils::sourceDirectory('tools/analysis', recursive = TRUE)
-	R.utils::sourceDirectory('tools/data', recursive = TRUE)
-	R.utils::sourceDirectory('tools/app', recursive = TRUE)
+	R.utils::sourceDirectory('tools/analysis', recursive = TRUE, modifiedOnly = FALSE)
+	R.utils::sourceDirectory('tools/data', recursive = TRUE, modifiedOnly = FALSE)
+	R.utils::sourceDirectory('tools/app', recursive = TRUE, modifiedOnly = FALSE)
 	# R.utils::sourceDirectory('../base/tools/data', recursive = TRUE)
 	# R.utils::sourceDirectory('../base/tools/app', recursive = TRUE)
 
